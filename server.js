95722857a86c8629d40bedd1c85a3dbce0467616
Dean <deandekaraokeguy@gmail.com>
2026-07-03 07:22:23 -0400

    fix mobile access binding

--- server.js
+++ server.js
@@ -964,6 +964,6 @@ socket.on("disconnect", () => {
    START SERVER
 ========================= */
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () =>
-  console.log("Server running on port " + PORT)
-);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
+});
